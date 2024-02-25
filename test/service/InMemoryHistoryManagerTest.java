5ce0ee1a0d17bb9e1c71d718c26795a02d8dbbd6
Roman Kopytov <glazinet1209@yandex.ru>
2024-02-25 17:59:18 +0300

    Исправил проверку на удаление Task

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -2,6 +2,7 @@ package service;
 
 import model.Status;
 import model.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @DisplayName("HistoryManager")
 class InMemoryHistoryManagerTest {
 
+    private static HistoryManager historyManager;
+    private static InMemoryTaskManager taskManager;
+
+    @BeforeEach
+    public void beforeEach() {
+        historyManager = new InMemoryHistoryManager();
+        taskManager = new InMemoryTaskManager(historyManager);
+    }
 
     @Test
     @DisplayName("Должна быть добавлена задача")
     void shouldAddTask() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-        InMemoryTaskManager taskManager = new InMemoryTaskManager(historyManager);
         Task task = new Task("First", Status.NEW, "Creation");
         taskManager.createTask(task);
         historyManager.add(task);
@@ -30,8 +37,6 @@ class InMemoryHistoryManagerTest {
     @Test
     @DisplayName("Должен выводить задачи в правильной последовательности")
     void shouldReturnInCorrectOrder() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-        InMemoryTaskManager taskManager = new InMemoryTaskManager(historyManager);
         Task task = new Task("First", Status.NEW, "Creation");
         Task taskTwo = new Task("Two", Status.NEW, "Creation");
         Task taskThree = new Task("Three", Status.NEW, "Creation");
@@ -50,26 +55,29 @@ class InMemoryHistoryManagerTest {
     @Test
     @DisplayName("Должен удалять задачи")
     void shouldDeleteTask() {
-        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
-        InMemoryTaskManager taskManager = new InMemoryTaskManager(historyManager);
         Task task = new Task("First", Status.NEW, "Creation");
         Task taskTwo = new Task("Two", Status.NEW, "Creation");
         Task taskThree = new Task("Three", Status.NEW, "Creation");
+        Task taskFour = new Task("Four", Status.NEW, "Creation");
+        Task taskFive = new Task("Five", Status.NEW, "Creation");
         taskManager.createTask(task);
         taskManager.createTask(taskTwo);
         taskManager.createTask(taskThree);
+        taskManager.createTask(taskFour);
+        taskManager.createTask(taskFive);
         taskManager.getTask(1);
         taskManager.getTask(2);
-        taskManager.getTask(3);
-        historyManager.remove(1);
+        taskManager.deleteTask(1);
         assertEquals(taskTwo, historyManager.getHistory().getFirst(), "Удалена не первая задача");
 
-        taskManager.getTask(1);
-        historyManager.remove(1);
-        assertEquals(taskThree, historyManager.getHistory().getLast(), "Удалена не последняя задача");
+        taskManager.getTask(3);
+        taskManager.deleteTask(3);
+        assertEquals(taskTwo, historyManager.getHistory().getLast(), "Удалена не последняя задача");
 
-        taskManager.getTask(1);
-        historyManager.remove(3);
-        assertEquals(task, historyManager.getHistory().get(1), "Удалена задача не из центра");
+
+        taskManager.getTask(4);
+        taskManager.getTask(5);
+        taskManager.deleteTask(4);
+        assertEquals(taskFive, historyManager.getHistory().get(1), "Удалена задача не из центра");
     }
 }
